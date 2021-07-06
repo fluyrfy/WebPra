96057d3906366ebf8b5054e456fcb866a3f3b1f8
Fan's Desk <frank880703@gmail.com>
2021-07-06 21:15:43 +0800

    20210706
    $toast,$messagebox

--- webPra/jsPra/VUE/VUEUI/vue_app_000/vue_app_00/src/components/exam/Exam02.vue
+++ webPra/jsPra/VUE/VUEUI/vue_app_000/vue_app_00/src/components/exam/Exam02.vue
@@ -2,6 +2,8 @@
   <div>
      <h1>Exam02.vue</h1>
      <button @click="handle1">点击完成操作</button>
+     <button @click="handle2">顯示交互式提示框</button>
+     <button @click="handle3">顯示確認框</button>
   </div>
 </template>
 <script>
@@ -12,10 +14,22 @@ export default {
       //console.log(123);
       this.$toast(
         {
-          message:"删除成功",
-          position:"bottom"
+          message:"删除成功", //顯示消息內容
+          position:"middle",//顯示位置
+          iconClass:"iconfont icon-chenggong"//顯示圖標(成功)
         })
-    }
+    },
+    handle2(){
+      //顯示交互式提示框
+      this.$messagebox("消息！","數據加載成功")//簡寫,標題可省略
+    },
+    handle3(){
+      //確認提示框
+      this.$messagebox.confirm("是否刪除指定數據")//確認內容
+      .then(res=>//確認執行成功後回調用
+      {console.log(res)}).catch(err=>{console.log(err)})//取消後調用;
+    },
+
   }
 }
 </script>
--- webPra/jsPra/VUE/VUEUI/vue_app_000/vue_app_00/src/router.js
+++ webPra/jsPra/VUE/VUEUI/vue_app_000/vue_app_00/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
 import Home from "./components/weixin/Home.vue"
 import Exam01 from "./components/exam/Exam01.vue" //引入自訂義組件 名字定義為Exam01
+import Exam02 from "./components/exam/Exam02.vue" //引入自訂義組件 名字定義為Exam01
 
 Vue.use(Router)
 export default new Router({
@@ -10,5 +11,6 @@ export default new Router({
     {path:'/Home',component:Home},
     {path:'/',component:HelloContainer},
     {path:'/Exam01',component:Exam01},
+    {path:'/Exam02',component:Exam02},
   ]
 })
